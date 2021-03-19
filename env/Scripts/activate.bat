0abf06c823f52d0132744f43133843de25e40397
noor.mammadov <noor.mammadov@gmail.com>
2021-03-20 00:53:58 +0400

    ....aydan change

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Python\FLASK\conference\env
+set VIRTUAL_ENV=C:\Users\aydan\Desktop\github\conference\env
 
 if not defined PROMPT set PROMPT=$P$G
 
