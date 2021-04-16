cbc9373543abb974a085f3037ea85959554f8115
noor.mammadov <noor.mammadov@gmail.com>
2021-04-16 10:35:14 +0400

    reviewer paper rating and auhor add paper style fixed

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template,flash, request, redirect, url_for, session
-from flask_mysqldb import MySQL,MySQLdb
+from flask import Flask, render_template, flash, request, redirect, url_for, session
+from flask_mysqldb import MySQL, MySQLdb
 import bcrypt
 
 app = Flask(__name__)
 app.secret_key = 'sakoblexeyible'
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'noor'#'noor''noor''noor'#'aydan'
-app.config['MYSQL_PASSWORD'] ='noor123'#'noor123'# # 'noor123' 'noor123' 'noor123' "a1w2k3i4m5.."
+app.config['MYSQL_USER'] = 'noor'  # 'noor''noor''noor'#'aydan'
+# 'noor123'# # 'noor123' 'noor123' 'noor123' "a1w2k3i4m5.."
+app.config['MYSQL_PASSWORD'] = 'noor123'
 app.config['MYSQL_DB'] = 'conference'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
+
 
 @app.route('/', methods=["GET", "POST"])
 def home():
@@ -22,26 +24,26 @@ def home():
         curl.execute("SELECT * FROM authors1 ")
         authors = curl.fetchall()
         curl.execute("SELECT * FROM papers1 ")
-        papers=curl.fetchall()
+        papers = curl.fetchall()
 
         curl.execute("SELECT * FROM interests1")
-        interests=curl.fetchall()
+        interests = curl.fetchall()
 
         print("interests list --> ", interests)
 
         curl.execute("SELECT * FROM chief_editor")
-        chief_editor=curl.fetchall()
+        chief_editor = curl.fetchall()
         chief_editor = True if len(chief_editor) > 0 else False
-                        
+
         curl.close()
 
-        return render_template("home.html", data=rewievers, authors=authors,papers=papers,chief_editor=chief_editor, interests=interests)
+        return render_template("home.html", data=rewievers, authors=authors, papers=papers, chief_editor=chief_editor, interests=interests)
     else:
         print("alalalal")
         return render_template("home.html")
 
 
-@app.route('/authors',methods=["GET", "POST"])
+@app.route('/authors', methods=["GET", "POST"])
 def authors():
     if request.method == "GET":
         curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
@@ -54,44 +56,45 @@ def authors():
         return render_template("home.html")
 
 
-@app.route('/rewievers',methods=["GET", "POST"])
+@app.route('/rewievers', methods=["GET", "POST"])
 def reviewers():
     if request.method == 'POST':
 
         firstname = request.form['firstname']
-        lastname=request.form['lastname']
+        lastname = request.form['lastname']
         email = request.form['email']
         interest_id = int(request.form['interest_id'])
         password = request.form['password'].encode('utf-8')
         hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
 
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO reviewers1 (firstname, lastname, interest_id, email, password) VALUES (%s,%s,%s,%s,%s)",( firstname,lastname,interest_id,email, hash_password))
+        cur.execute("INSERT INTO reviewers1 (firstname, lastname, interest_id, email, password) VALUES (%s,%s,%s,%s,%s)",
+                    (firstname, lastname, interest_id, email, hash_password))
         mysql.connection.commit()
         return render_template("home.html")
     else:
 
         return render_template("home.html")
 
-@app.route('/chief_editor',methods=["GET", "POST"])
+
+@app.route('/chief_editor', methods=["GET", "POST"])
 def chief_editor():
     if request.method == 'POST':
 
         firstname = request.form['firstname']
-        lastname=request.form['lastname']
+        lastname = request.form['lastname']
         email = request.form['email']
         password = request.form['password'].encode('utf-8')
         hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
 
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO chief_editor (firstname, lastname, email, password) VALUES (%s,%s,%s,%s)",(firstname,lastname, email, hash_password))
+        cur.execute("INSERT INTO chief_editor (firstname, lastname, email, password) VALUES (%s,%s,%s,%s)",
+                    (firstname, lastname, email, hash_password))
         mysql.connection.commit()
         return render_template("home.html")
     else:
         print("lalal")
         return render_template("home.html")
-
-
 
 
 @app.route('/logout', methods=["GET", "POST"])
@@ -100,6 +103,7 @@ def logout():
     session.clear()
     flash("You successfully logged out")
     return render_template("home.html")
+
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
@@ -117,11 +121,13 @@ def register():
         hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
 
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO authors1 (firstname,lastname, phone, email, country, city,zipcode, password) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",(firstname,lastname, phone, email, country, city,zip, hash_password))
+        cur.execute("INSERT INTO authors1 (firstname,lastname, phone, email, country, city,zipcode, password) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",
+                    (firstname, lastname, phone, email, country, city, zip, hash_password))
         mysql.connection.commit()
         session['name'] = request.form['firstname']
         session['email'] = request.form['email']
         return redirect(url_for('home'))
+
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -129,9 +135,8 @@ def login():
         email = request.form['email']
         password = request.form['password'].encode('utf-8')
 
-
         curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        curl.execute("SELECT * FROM admins WHERE email=%s",(email,))
+        curl.execute("SELECT * FROM admins WHERE email=%s", (email,))
         user = curl.fetchone()
         curl.close()
 
@@ -148,21 +153,22 @@ def login():
     else:
         return render_template("login.html")
 
+
 @app.route('/login_author', methods=["GET", "POST"])
 def login_author():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password'].encode('utf-8')
 
-        print("email --> ",password)
-        print("password --> ",password)
+        print("email --> ", password)
+        print("password --> ", password)
 
         curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        curl.execute("SELECT * FROM authors1 WHERE email=%s",(email,))
+        curl.execute("SELECT * FROM authors1 WHERE email=%s", (email,))
         user = curl.fetchone()
-        print("User --> ",user)
+        print("User --> ", user)
         curl.close()
-        
+
         if len(user) > 0:
             if bcrypt.hashpw(password, user["password"].encode('utf-8')) == user["password"].encode('utf-8'):
                 session['name'] = user['firstname']
@@ -174,9 +180,10 @@ def login_author():
 
                 curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
                 author_id = session['id']
-                
+
                 # curl.execute("SELECT * FROM papers1 WHERE author_id=%s",(author_id,))
-                curl.execute("SELECT * FROM papers1 LEFT JOIN interests1 using(interest_id) WHERE author_id=%s",(author_id,))
+                curl.execute(
+                    "SELECT * FROM papers1 LEFT JOIN interests1 using(interest_id) WHERE author_id=%s", (author_id,))
 
                 papers = curl.fetchall()
 
@@ -203,30 +210,28 @@ def rating():
         paper_id = request.form["submit_b"]
         reviewer_id = session["reviewer_id"]
         curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        curl.execute("INSERT INTO paper_status1 (reviewer_id, paper_id, rating, comment) VALUES (%s,%s,%s,%s)",(reviewer_id,paper_id,rating,comment, ))
+        curl.execute("INSERT INTO paper_status1 (reviewer_id, paper_id, rating, comment) VALUES (%s,%s,%s,%s)",
+                     (reviewer_id, paper_id, rating, comment, ))
         mysql.connection.commit()
-        result = curl.fetchall();
-        print("rating --> ", rating, "comment -->", comment, "id -->", paper_id, "reviewer id --> ", reviewer_id);
+        result = curl.fetchall()
+        print("rating --> ", rating, "comment -->", comment,
+              "id -->", paper_id, "reviewer id --> ", reviewer_id)
         print("rating result --->", result)
 
-
-        # return redirect(url_for('login_reviewer'))
-        return render_template("reviewers.html")
-
+        return redirect(url_for('reviewer_page'))
+        # return render_template("reviewers.html")
 
 
 @app.route('/login_reviewer', methods=["GET", "POST"])
 def login_reviewer():
+    if session.get('rewiever_id'):
+        return redirect(url_for('reviewer_page'))
     if request.method == 'POST':
-        email= request.form['email']
+        email = request.form['email']
         password = request.form['password'].encode('utf-8')
-
         curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        curl.execute("SELECT * FROM reviewers1 WHERE email=%s",(email,))
-        
-        
+        curl.execute("SELECT * FROM reviewers1 WHERE email=%s", (email,))
         user = curl.fetchone()
-        
         curl.close()
         print("beafore if ---> ", session)
         if len(user) > 0:
@@ -238,8 +243,8 @@ def login_reviewer():
                 session['reviewer_id'] = user['reviewer_id']
                 session['interest_id'] = user['interest_id']
 
-                rew_id = session['reviewer_id'];
-                int_id = session['interest_id'];
+                rew_id = session['reviewer_id']
+                int_id = session['interest_id']
 
                 print("+_+_+_+_+_+_+_+_+_+_+", rew_id, int_id)
                 curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
@@ -252,13 +257,20 @@ def login_reviewer():
                 curl.execute("SELECT * FROM papers1")
                 authors = curl.fetchall()
 
-                curl.execute("SELECT Distinct papers1.paper_id, papers1.abstract, authors1.firstname,authors1.lastname, papers1.title, papers1.body, interests1.interest_name FROM papers1 INNER JOIN authors1 INNER JOIN reviewers1 INNER JOIN interests1 ON (papers1.author_id = authors1.author_id AND papers1.interest_id = %s AND reviewers1.reviewer_id=%s AND interests1.interest_id=%s)", (int_id,rew_id,int_id,))
+                curl.execute("SELECT Distinct papers1.paper_id, papers1.abstract, authors1.firstname,authors1.lastname, papers1.title, papers1.body, interests1.interest_name FROM papers1 INNER JOIN authors1 INNER JOIN reviewers1 INNER JOIN interests1 ON (papers1.author_id = authors1.author_id AND papers1.interest_id = %s AND reviewers1.reviewer_id=%s AND interests1.interest_id=%s)", (int_id, rew_id, int_id,))
                 answer = curl.fetchall()
-                print("answer ----++++++++++++_________++++++++++++++++++----------------+++++++++++++++++++", answer)
+                print(
+                    "answer ----++++++++++++_________++++++++++++++++++----------------+++++++++++++++++++", answer)
 
-                
-                return render_template("reviewers.html", firstname=user['lastname'], papers=papers, authors=authors, answer=answer)
-                # return redirect(url_for('login_reviewer', firstname=user['lastname'], papers=papers, authors=authors) )
+                data = {
+                    "firstname": user['lastname'],
+                    "papers": papers,
+                    "authors": authors,
+                    "answer":answer
+                }
+                session['data'] = data
+                # return render_template("reviewers.html", firstname=user['lastname'], papers=papers, authors=authors, answer=answer)
+                return redirect(url_for('reviewer_page') )
             else:
                 return render_template('error.html')
         else:
@@ -268,11 +280,21 @@ def login_reviewer():
         return render_template("login.html")
 
 
-
+@app.route('/reviewer_page', methods=["GET", "POST"])
+def reviewer_page():
+    if session.get('reviewer_id') != None:
+        if session['data']:
+            firstname = session['data']['firstname']
+            papers = session['data']['papers']
+            authors = session['data']['authors']
+            answer = session['data']['answer']
+        return render_template("reviewers.html",firstname=firstname, papers=papers, authors=authors, answer=answer )
+    else:
+        redirect(url_for("login_reviewer"))
 
 
 @app.route('/submit_paper', methods=["GET", "POST"])
-def submit_paper(): ##database name is papers
+def submit_paper():  # database name is papers
     if request.method == 'GET':
         return render_template("author.html")
     else:
@@ -284,14 +306,13 @@ def submit_paper(): ##database name is papers
         body = request.form['body']
 
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO papers1 (title, interest_id, author_id, keywords, abstract, body) VALUES (%s,%s,%s,%s,%s,%s)",(title,interest_id,author_id,keywords,abstract,body, ))
+        cur.execute("INSERT INTO papers1 (title, interest_id, author_id, keywords, abstract, body) VALUES (%s,%s,%s,%s,%s,%s)",
+                    (title, interest_id, author_id, keywords, abstract, body, ))
         mysql.connection.commit()
         return redirect(url_for('submit_paper'))
 
 
-
-
-@app.route('/papers',methods=["GET", "POST"])
+@app.route('/papers', methods=["GET", "POST"])
 def papers():
     if request.method == "GET":
         curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
@@ -302,7 +323,7 @@ def papers():
         return render_template("login.html", papers=papers)
 
 
-@app.route('/papers1',methods=["GET", "POST"])
+@app.route('/papers1', methods=["GET", "POST"])
 def get_papers():
     if request.method == "GET":
         curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
@@ -314,51 +335,45 @@ def get_papers():
         # return render_template("reviewers.html", papers1=papers1)
 
 
-
-
-@app.route('/delete_rewiever/<id>/',methods=["GET", "POST"])
+@app.route('/delete_rewiever/<id>/', methods=["GET", "POST"])
 def delete_rewiever(id):
-        print("Delete id --> ", id)
-        curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        curl.execute("DELETE from rewievers WHERE id=%s",(id,))
-        curl.close()
-        mysql.connection.commit()
-        return redirect(url_for('home'))
+    print("Delete id --> ", id)
+    curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    curl.execute("DELETE from rewievers WHERE id=%s", (id,))
+    curl.close()
+    mysql.connection.commit()
+    return redirect(url_for('home'))
 
 
-@app.route('/update_rewiever/<int:id>/', methods = ['GET', 'POST'])
+@app.route('/update_rewiever/<int:id>/', methods=['GET', 'POST'])
 def update_rewiever(id):
 
     firstname = request.form['firstname']
     lastname = request.form['lastname']
-   
+
     email = request.form['email']
 
-    print("------->>>>>>>",firstname, lastname, email)
+    print("------->>>>>>>", firstname, lastname, email)
     curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     # curl.execute("SELECT * from rewievers WHERE id=%s",(id,))
 
     sql = ("UPDATE rewievers SET firstname=%s, lastname=%s, email=%s WHERE id = %s")
-    val = ( firstname, lastname, email, id)
+    val = (firstname, lastname, email, id)
     curl.execute(sql, val)
     curl.close()
     mysql.connection.commit()
- 
+
     return redirect(url_for('home'))
 
 
-
-
-
 @app.route('/direct', methods=["GET"])
-def direct_pages(): ##database name is papers
+def direct_pages():  # database name is papers
     if request.method == "GET":
         curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         curl.execute("SELECT * FROM authors1 ")
         authors = curl.fetchall()
         curl.close()
         return render_template("view.html", authors=authors)
-
 
 
 if __name__ == '__main__':
