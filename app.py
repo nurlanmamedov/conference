32b41965033565cf5e5da3dccb242e504cf7c42a
aydawka <aydanarxoz_5@mail.ru>
2021-04-16 23:34:03 -0500

    author modal changed

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ app.secret_key = 'sakoblexeyible'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'noor'  # 'noor''noor''noor'#'aydan'
-# 'noor123'# # 'noor123' 'noor123' 'noor123' "a1w2k3i4m5.."
-app.config['MYSQL_PASSWORD'] = 'noor123'
+app.config['MYSQL_PASSWORD'] = 'noor123'# 'noor123'# # 'noor123' 'noor123' 'noor123' "a1w2k3i4m5.."
 app.config['MYSQL_DB'] = 'conference'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
