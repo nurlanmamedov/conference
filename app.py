e768f872908894ca3cf5c967114317069f2e8209
aydawka <aydanarxoz_5@mail.ru>
2021-04-26 09:03:28 -0500

    xirda

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 app.secret_key = 'sakoblexeyible'
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'aydan'  # 'noor'#'aydan'
-app.config['MYSQL_PASSWORD'] = 'a1w2k3i4m5..'# "a1w2k3i4m5.."'noor123'
+app.config['MYSQL_USER'] = 'nurlan'  # 'noor'#'aydan'
+app.config['MYSQL_PASSWORD'] = 'nurlan123'# "a1w2k3i4m5.."'noor123'
 app.config['MYSQL_DB'] = 'conference'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
@@ -72,7 +72,7 @@ def reviewers():
 
 
         if password != password2:
-            return render_template("error.html")
+            return render_template("match.html")
         hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
 
         cur = mysql.connection.cursor()
@@ -146,7 +146,7 @@ def register():
         password2 = request.form['password2'].encode('utf-8')
 
         if password != password2:
-            return render_template("error.html")
+            return render_template("match.html")
         hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
 
         cur = mysql.connection.cursor()
